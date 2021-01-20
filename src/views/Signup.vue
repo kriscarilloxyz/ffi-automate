38e6dfea9206636376ab29e4bc42797498af8c68
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:16:27 +0800

    added signup view

---
+++ src/views/Signup.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container id="Signup"
+               fill-height>
+    <v-row align-center>
+      <v-col cols="12">
+        <card-signup class="mx-auto" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import cardSignup from '@/components/cardSignup'
+
+export default {
+  name: 'Signup',
+  components: { cardSignup }
+}
+</script>
