df8e6369a6b89c300ec0791736031c6448dad2cd
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:12:31 +0800

    display loading view

---
+++ src/components/displayLoading.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fill-height>
+        <v-row>
+          <v-col cols="12">
+            <v-card max-width="400"
+                    class="mx-auto">
+              <v-card-text>
+                <v-progress-linear indeterminate></v-progress-linear>
+              </v-card-text>
+            </v-card>
+
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
