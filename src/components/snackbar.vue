4958f6848765652a91d7d979a2d237eb72bdfd38
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:14:58 +0800

    snackbar component

---
+++ src/components/snackbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-snackbar id="snackbar"
+              v-model="snackbar.state"
+              :color="snackbar.color"
+              top
+              right>
+    {{snackbar.message}}
+  </v-snackbar>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Snackbar',
+
+  computed: {
+    ...mapState([
+      'snackbar'
+    ]),
+
+
+    state: {
+      get () { return this.snackbar.state },
+      set (v) { this.$store.commit('set_snackbar', { state: v }) }
+    }
+  }
+}
+</script>
