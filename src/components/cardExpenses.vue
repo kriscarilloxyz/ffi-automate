8efcb5d72bcd531d1e9968d7711d9763bb16828d
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:11:52 +0800

    card expenses component

---
+++ src/components/cardExpenses.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Expenses
+    </v-card-title>
+    <v-card-text>
+      <h1 class="display-2">
+        $ 0.00
+      </h1>
+    </v-card-text>
+    <v-divider class="mt-12"></v-divider>
+    <v-card-actions>
+      <v-btn text
+             disabled>
+        <v-icon>mdi-menu-down</v-icon>
+        % 5.12
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
