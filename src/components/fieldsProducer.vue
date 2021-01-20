a395c70f26203e841a571dab0079dd37a1d9e3eb
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:14:29 +0800

    fields producer component

---
+++ src/components/fieldsProducer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fieldsProducer">
+    <v-text-field label="Name"
+                  v-model="producer.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-text-field label="Role"
+                  v-model="producer.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-file-input prepend-icon=""
+                  label="Image"
+                  v-model="producer.file"></v-file-input>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsProducer',
+  props: ['producer']
+}
+</script>
