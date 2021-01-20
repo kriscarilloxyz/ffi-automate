1b1e871f408965d5e7294b8a816eea181f707694
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:12:39 +0800

    fields actor component

---
+++ src/components/fieldsActor.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fieldsActor">
+    <v-text-field label="Name"
+                  v-model="actor.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-text-field label="Role"
+                  v-model="actor.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-file-input prepend-icon=""
+                  label="Image"
+                  v-model="actor.file"></v-file-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsActor',
+  props: ['actor']
+}
+</script>
