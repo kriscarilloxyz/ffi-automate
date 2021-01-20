7f4ba6fbd624227e06149245c8f05bdf70c92e2e
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:14:36 +0800

    fields signin component

---
+++ src/components/fieldsSignin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="fieldsSignin">
+    <v-text-field label="Email"
+                  v-model="form.email"
+                  :rules="$rule.email"
+                  type="email" />
+
+    <v-text-field label="Password"
+                  v-model="form.password"
+                  :rules="$rule.password"
+                  type="password" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsSignin',
+  props: ['form']
+}
+</script>
