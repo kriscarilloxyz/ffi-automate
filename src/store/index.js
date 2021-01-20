c287d516f125bfdfc1ac3f91b2942be093fbb2af
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:16:43 +0800

    snackbar, loading, user store state + mutations

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    snackbar: {},
+    loading: true,
+    user: false
   },
   mutations: {
+    set_snackbar (state, payload) { state.snackbar = payload },
+    set_loading (state, payload) { state.loading = payload },
+    set_user (state, payload) { state.user = payload }
   },
   actions: {
   },
