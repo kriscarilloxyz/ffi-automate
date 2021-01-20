af3efa2aa243de8bb0d22ab78db40fb39431d6c2
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:12:57 +0800

    fields award component

---
+++ src/components/fieldsAward.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fieldsAward">
+    <v-text-field label="Name"
+                  v-model="award.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-file-input prepend-icon=""
+                  label="Image / Poster"
+                  v-model="award.file"></v-file-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsAward',
+  props: ['award']
+}
+</script>
