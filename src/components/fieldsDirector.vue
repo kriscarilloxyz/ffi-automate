fa17f389148df560edeb5ee3b672516fb2a5ba0c
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:13:04 +0800

    fields director component

---
+++ src/components/fieldsDirector.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="fieldsDirector">
+    <v-text-field label="Name"
+                  v-model="director.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-text-field label="Role"
+                  v-model="director.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-file-input prepend-icon=""
+                  label="Image"
+                  v-model="director.file"></v-file-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsDirector',
+  props: ['director']
+}
+</script>
