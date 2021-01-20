2c4ba0072ceed526d6a838ac8fb4b39c997dc58b
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:14:42 +0800

    fields signup component component

---
+++ src/components/fieldsSignup.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="fieldsSignup">
+    <v-text-field label="Email"
+                  v-model="form.email"
+                  :rules="$rule.email"
+                  type="email"></v-text-field>
+
+    <v-text-field label="Password"
+                  v-model="form.password"
+                  :rules="$rule.password"
+                  type="password"></v-text-field>
+
+    <v-text-field label="Full name"
+                  v-model="form.name"
+                  :rules="$rule.required"
+                  type="text">
+    </v-text-field>
+
+    <v-text-field label="Phone Number"
+                  v-model="form.phone"
+                  :rules="$rule.required"
+                  type="text">
+    </v-text-field>
+
+    <v-text-field label="Address Line"
+                  v-model="form.address"
+                  :rules="$rule.required"
+                  type="text">
+    </v-text-field>
+
+    <v-text-field label="City"
+                  v-model="form.city"
+                  :rules="$rule.required"
+                  type="text">
+    </v-text-field>
+
+    <v-text-field label="State / Province / Region"
+                  v-model="form.state"
+                  :rules="$rule.required"
+                  type="text">
+    </v-text-field>
+
+    <v-text-field label="Zip / Postal Code"
+                  v-model="form.zip"
+                  :rules="$rule.required"
+                  type="text">
+    </v-text-field>
+
+    <v-autocomplete ref="country"
+                    v-model="form.country"
+                    :rules="$rule.required"
+                    :items="countries"
+                    label="Country"
+                    placeholder="Select..."
+                    required></v-autocomplete>
+
+    <v-text-field label="Company"
+                  v-model="form.company"
+                  :rules="$rule.required"
+                  type="text">
+    </v-text-field>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsSignup',
+  props: ['form']
+}
+</script>
