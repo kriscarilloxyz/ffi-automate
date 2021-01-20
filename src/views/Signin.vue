6feee7ec42193666c390126b0514fab1d61842b5
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:16:22 +0800

    added signin view

---
+++ src/views/Signin.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fill-height
+               id="Signin">
+    <v-row justify-center>
+      <v-col cols="12">
+        <card-signin class="mx-auto"></card-signin>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import cardSignin from '../components/cardSignin.vue'
+
+export default {
+  name: 'Signin',
+
+  components: { cardSignin }
+}
+</script>
