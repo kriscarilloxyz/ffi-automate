d9fe19e76a8827894737cdd3c2f68a9cd66680c9
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:13:15 +0800

    fields festival component

---
+++ src/components/fieldsFestival.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="fieldsFestival">
+    <v-text-field label="Name"
+                  v-model="festival.name"
+                  :rules="$rule.required"></v-text-field>
+
+    <v-file-input prepend-icon=""
+                  label="Image / Poster"
+                  v-model="festival.file"></v-file-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsFestival',
+  props: ['festival']
+}
+</script>
