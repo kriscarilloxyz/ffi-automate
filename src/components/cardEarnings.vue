42fe947905357599921d555cabcd0c24031f9eaf
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:11:45 +0800

    card earnings component

---
+++ src/components/cardEarnings.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Earnings
+    </v-card-title>
+    <v-card-text>
+      <h1 class="display-2">
+        $ 0.00
+      </h1>
+    </v-card-text>
+    <v-divider class="mt-12"></v-divider>
+    <v-card-actions>
+      <v-btn disabled
+             color="primary">
+        Withdraw
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
