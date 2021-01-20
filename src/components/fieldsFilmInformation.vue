4471480f732f16d7f7a58922c8693873cd410889
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:14:16 +0800

    fields film information component

---
+++ src/components/fieldsFilmInformation.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="fieldsFilmInformation">
+    <v-text-field label="Film name"
+                  v-model="form.name"
+                  type="text"
+                  :rules="$rule.required" />
+
+    <v-text-field label="Year Released"
+                  v-model="form.year"
+                  type="number"
+                  :rules="$rule.required" />
+
+    <v-text-field label="Running time"
+                  persistent-hint
+                  v-model="form.runningTime"
+                  type="number"
+                  :rules="$rule.required" />
+
+    <v-autocomplete ref="country"
+                    v-model="form.productionCountry"
+                    :rules="$rule.required"
+                    :items="countries"
+                    label="Production Country"
+                    placeholder="Select..." />
+
+    <v-combobox multiple
+                label="Genre"
+                placeholder="Hit enter to add an item"
+                v-model="form.genres"
+                :rules="$rule.required"
+                chips></v-combobox>
+
+    <v-textarea label="Description"
+                v-model="form.descripton"
+                :rules="$rule.required"></v-textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'fieldsFilmInformation',
+  props: ['form']
+}
+</script>
