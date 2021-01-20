46af95bddc57255ea18a067b5d271da0d8905226
kriscarilloxyz <kriscarilloxyz@gmail.com>
2021-01-21 01:12:04 +0800

    card sales component

---
+++ src/components/cardSales.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Sales
+    </v-card-title>
+    <v-card-text>
+      <h1 class="display-2">
+        $ 0.00
+      </h1>
+    </v-card-text>
+    <v-divider class="mt-12"></v-divider>
+    <v-card-actions>
+      <v-btn text
+             disabled>
+        <v-icon>mdi-menu-up</v-icon>
+        % 9.15
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
